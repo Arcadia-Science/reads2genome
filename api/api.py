380ee36ca6999452f3b8107822c1baab78e6d655
Feridun Mert Celebi <mertcelebi93@gmail.com>
2023-08-03 15:58:34 -0400

    Fix the API

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ api_image = Image.debian_slim().pip_install("requests==2.26.0")
 
 @stub.function(secret=Secret.from_name("nf-tower-secrets"), image=api_image)
 @web_endpoint()
-def run_pipeline(uri: str, lineage: str = "chlorophyta_odb10", platform: str = "nanopore"):
+def run_pipeline(input: str, outdir: str, lineage: str = "chlorophyta_odb10", platform: str = "nanopore"):
     formatted_date = datetime.now().strftime("%Y-%m-%d")
     multiqc_title = f"{formatted_date} - MultiQC Report"
 
@@ -22,8 +22,8 @@ def run_pipeline(uri: str, lineage: str = "chlorophyta_odb10", platform: str = "
         "params": {
             "from_email": os.environ["FROM_EMAIL"],
             "email": os.environ["NOTIFICATION_EMAIL"],
-            "input": uri,
-            "outdir": "s3://nf-hifi2genome/2023-08-03-merttest",
+            "input": input,
+            "outdir": outdir,
             "lineage": lineage,
             "multiqc_title": multiqc_title,
             "platform": platform,
