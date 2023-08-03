6493eecc06d1c8c498788cd69c762de9e6c146ff
Feridun Mert Celebi <mertcelebi93@gmail.com>
2023-08-03 14:44:22 -0400

    Parametrize the whole thing

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ api_image = Image.debian_slim().pip_install("requests==2.26.0")
 
 @stub.function(secret=Secret.from_name("nf-tower-secrets"), image=api_image)
 @web_endpoint()
-def nanopore_run(uri: str):
+def run_pipeline(uri: str, lineage: str = "chlorophyta_odb10", platform: str = "nanopore"):
     formatted_date = datetime.now().strftime("%Y-%m-%d")
     multiqc_title = f"{formatted_date} - MultiQC Report"
 
@@ -23,9 +23,9 @@ def nanopore_run(uri: str):
             "from_email": os.environ["FROM_EMAIL"],
             "email": os.environ["NOTIFICATION_EMAIL"],
             "input": uri,
-            "lineage": "chlorophyta_odb10",
+            "lineage": lineage,
             "multiqc_title": multiqc_title,
-            "platform": "nanopore",
+            "platform": platform,
         },
     }
 
